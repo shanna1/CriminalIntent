43ca260e9e6121e984448173e444a954667438fd
Spencer <shanna@gustavus.edu>
2016-02-26 18:42:17 -0600

    Chapter 7

--- app/src/main/java/com/bigneardranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bigneardranch/android/criminalintent/CrimeActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 
 public class CrimeActivity extends FragmentActivity {
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
