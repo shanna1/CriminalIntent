cef7323b3b16d1cbe66faa593c08e9ea553cf0dc
Spencer <shanna@gustavus.edu>
2016-02-26 20:21:26 -0600

    Chapter 8

--- app/src/main/java/com/bigneardranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bigneardranch/android/criminalintent/CrimeActivity.java
@@ -1,10 +1,9 @@
 package com.bigneardranch.android.criminalintent;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
 
