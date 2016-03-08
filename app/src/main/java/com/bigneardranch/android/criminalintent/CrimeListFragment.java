35bd86eaa6189a6bfd04eb43e4d8fc2f210aa2f9
Spencer <shanna@gustavus.edu>
2016-03-08 11:56:03 -0600

    CH 11 (through 11.5)

--- app/src/main/java/com/bigneardranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bigneardranch/android/criminalintent/CrimeListFragment.java
@@ -82,7 +82,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v){
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newInent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
 
