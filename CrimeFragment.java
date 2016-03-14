49472424b746c9381c6a9d2f2bbaa637ea34c389
shanna1 <shanna@gustavus.edu>
2016-03-13 19:24:05 -0500

    Added files via upload

--- CrimeFragment.java
+++ CrimeFragment.java
@@ -1,38 +1,75 @@
 package com.bigneardranch.android.criminalintent;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.text.format.DateFormat;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import java.util.Date;
+import java.util.UUID;
+
 
 public class CrimeFragment extends Fragment {
+
+    private static final String DIALOG_DATE = "DialogDate";
+    private static final String ARG_CRIME_ID = "crime_id";
+
+    private static final int REQUEST_DATE = 0;
+
     private Crime mCrime;
+    private Button mDateButton;
+    private AlertDialog Dialog;
+
+    public static CrimeFragment newInstance(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID, crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
+        setHasOptionsMenu(true);
+        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
+                             Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
-        EditText titleField = (EditText) v.findViewById(R.id.crime_title);
-        titleField.addTextChangedListener(new TextWatcher() {
+
+
+        EditText mtitleField = (EditText) v.findViewById(R.id.crime_title);
+        mtitleField.setText(mCrime.getTitle());
+        mtitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                //This space intentionally left blank
+
             }
 
             @Override
@@ -47,22 +84,96 @@ public class CrimeFragment extends Fragment {
             }
         });
 
-        //These lines of code formats the date to look like "Wendnesday, Jul 22, 2015" as in the book
 
-        Button dateButton = (Button) v.findViewById(R.id.crime_date);
-        DateFormat formatter = new DateFormat();
-        dateButton.setText(DateFormat.format("EEEE, MMM dd, yyyy", new java.util.Date()));
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        updateDate();
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment.
+                        newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
 
 
-        CheckBox solvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
-        solvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        final CheckBox mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 //Set the crime's solved property
                 mCrime.setSolved(isChecked);
+
             }
         });
 
         return v;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+
+        mDateButton.setText(mCrime.getDate().toString());
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime, menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_delete_crime:
+                if (mCrime != null) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setTitle("Deletion Alert");
+                    builder.setMessage("Are you sure?");
+                    builder.setPositiveButton("Yes",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(getActivity(), "Deleting crime", Toast.LENGTH_SHORT).show();
+                                    CrimeLab.get(getActivity()).deleteCrime(mCrime);
+                                    getActivity().finish();
+                                }
+                            });
+                    builder.setNegativeButton("No",
+                            new DialogInterface.OnClickListener(){
+                                @Override
+                                public void onClick(DialogInterface dialog, int which){
+                                    dialog.cancel();
+                                }
+                            });
+                    builder.show();
+
+
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+
 }
