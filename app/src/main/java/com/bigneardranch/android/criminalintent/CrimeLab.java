f36c726d9f59474c31e2f7c902afa3265f42747d
Spencer <shanna@gustavus.edu>
2016-03-17 17:06:12 -0500

    CH 14 (pg 266)

---
+++ app/src/main/java/com/bigneardranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,71 @@
+package com.bigneardranch.android.criminalintent;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import database.CrimeBaseHelper;
+import database.CrimeDbSchema;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+    public static CrimeLab get(Context context){
+        if (sCrimeLab == null){
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context){
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
+
+    }
+
+    public void addCrime(Crime c){
+        ContentValues values = getContentValues(c);
+
+        mDatabase.insert(CrimeDbSchema.CrimeTable.NAME, null, values);
+
+    }
+
+    public List<Crime> getCrimes(){
+        return new ArrayList<>();
+    }
+
+    public Crime getCrime(UUID id){
+
+        return null;
+    }
+
+    public void updateCrime(Crime crime){
+        String uuidString = crime.getId().toString();
+        ContentValues values = getContentValues(crime);
+
+        mDatabase.update(CrimeDbSchema.CrimeTable.NAME, values, CrimeDbSchema.CrimeTable.Cols.UUID + " = ?", new String[] { uuidString});
+    }
+
+    public void deleteCrime(Crime crime){
+
+
+    }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+        values.put(CrimeDbSchema.CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeDbSchema.CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeDbSchema.CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeDbSchema.CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+
+        return values;
+    }
+}
